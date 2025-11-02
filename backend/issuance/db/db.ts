22e0d8568082ab837bec5a93ab348e9105ad19cf
Harkirat Singh <singharkirath1511@gmail.com>
2025-11-02 23:51:32 +0530

    Implement database connection error handling and add Kubernetes configurations for issuance and verification services

--- backend/issuance/db/db.ts
+++ backend/issuance/db/db.ts
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 
-const connectDB=async()=>{
-    mongoose.connection.on('connected',()=>console.log("Database Connected"));
-    await mongoose.connect(`${process.env.MONGODB_URI}`, {
-        ssl: true
-    });
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => console.log("Database Connected"));
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}`, {
+
+        });
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        throw error;
+    }
 }
 
-const closeDatabase = async()=>{
+const closeDatabase = async () => {
     console.log("db called!");
+    await mongoose.connection.close();
 }
 
-export {connectDB, closeDatabase}
+export { connectDB, closeDatabase }
